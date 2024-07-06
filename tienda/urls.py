f18351d3bcfbf28e1a690c8ab343dd913edc76ee
CarinaTorres <ca.torresa@duocuc.cl>
2024-07-06 13:33:10 -0400

    Se agrega contacto

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base', views.base, name="base")
+   
+    path('base', views.base, name="base"),
+    path('contacto', views.base, name="contacto"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 def base(request):
     context = {}
     return render(request, 'base.html',context)
+
+
+def intex(request):
+    context = {}
+    return render(request, 'tienda.html',context)
+
+def contacto(request):
+    context = {}
+    return render(request, 'contacto.html',context)
